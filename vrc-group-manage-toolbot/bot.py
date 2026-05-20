943516575f608ed72c6998a40e3bfd13b1c3c88a
XChen446 <xchen446@outlook.com>
2026-05-20 20:42:22 +0800

    fix(startup): 启动脚本监听调度器启停以确保调度器可用

--- vrc-group-manage-toolbot/bot.py
+++ vrc-group-manage-toolbot/bot.py
@@ -1,6 +1,7 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter
-from nonebot import get_driver
+from nonebot import get_driver, logger
+from services.scheduler_service import scheduler_service
 
 # 初始化 NoneBot
 nonebot.init()
@@ -10,10 +11,15 @@ driver = get_driver()
 driver.register_adapter(Adapter)
 
 
+@driver.on_startup
+async def _startup():
+    scheduler_service.start_scheduler()
+
 @driver.on_shutdown
 async def _shutdown():
     from utils import get_vrc_client
     await get_vrc_client().close()
+    scheduler_service.shutdown_scheduler()
 
 
 app = nonebot.get_app()
